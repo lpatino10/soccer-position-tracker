99ad8d516db7057ecac1a41f358d831178b5f4a0
Logan Patino <loganpatino10@gmail.com>
2026-03-05 23:56:15 -0800

    Adjust normalized coordinates to set origin at top left of field

--- data-service/epts_parser.py
+++ data-service/epts_parser.py
@@ -22,8 +22,8 @@ bucket_base_url = "https://paugmeoshsaakpmuwsoo.supabase.co/storage/v1/object/pu
 
 def parse_game_data(game_id: str) -> list[dict[str, int]]:
   dataset = metrica.load_tracking_epts(
-    meta_data=f"{bucket_base_url}/{game_id}/metadata.xml?cache=v1",
-    raw_data=f"{bucket_base_url}/{game_id}/tracking.txt?cache=v1",
+    meta_data=f"{bucket_base_url}/{game_id}/metadata.xml?cache=v2",
+    raw_data=f"{bucket_base_url}/{game_id}/tracking.txt?cache=v2",
     coordinates="metrica",
   )
 
