2b0970d6ef0ee9501fc755a4c051738e694fad45
Logan Patino <loganpatino10@gmail.com>
2026-04-08 22:40:31 -0700

    Add some fun flavor text

--- playback-site/src/routes/+layout.svelte
+++ playback-site/src/routes/+layout.svelte
@@ -24,4 +24,9 @@
 	>
 		<p class="text-body text-sm">{`VER: ${version}`}</p>
 	</div>
+	<div
+		class="absolute rotate-90 right-0 bottom-0 origin-top-right -translate-x-1 text-body/25"
+	>
+		STATUS: ONLINE // LOGS: VERIFIED // POSITIONAL_DATA: SYNCED
+	</div>
 </div>
--- playback-site/src/routes/[gameId]/+page.svelte
+++ playback-site/src/routes/[gameId]/+page.svelte
@@ -50,10 +50,6 @@
 <style>
   .page-container {
     display: flex;
-    height: 100vh;
-    width: 100%;
-    /* align-items: center;
-    justify-content: center; */
   }
 
   .pitch {
